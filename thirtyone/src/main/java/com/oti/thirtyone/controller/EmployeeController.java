37771d371a1c126fa82eeaf7acf7bb0faecfeeb2
361_seok <jws4858@gmail.com>
2024-10-24 18:13:33 +0900

    Update: "사원 개인정보 수정 정적 페이지 추가(완료)"
    사원 개인정보 수정 정적 페이지 추가(완료)

---
+++ thirtyone/src/main/java/com/oti/thirtyone/controller/EmployeeController.java
@@ -0,0 +1,26 @@
+package com.oti.thirtyone.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Controller
+@RequestMapping("/emp")
+@Slf4j
+public class EmployeeController {
+	
+	@RequestMapping("loginForm")
+	public String loginForm() {
+		return "employee/loginForm";
+	}
+	
+	@GetMapping("empDetail")
+	public String empDetail(Model model){		
+		model.addAttribute("title", "정원석님의 정보 수정하기");
+		
+		return "employee/empDetail";
+	}
+}
