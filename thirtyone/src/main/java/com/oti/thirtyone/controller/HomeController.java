6de0fc0f259b1bb7560018f42f1a634053398ad5
361_seok <jws4858@gmail.com>
2024-10-26 16:23:12 +0900

    Update: "로그인 안되도 main페이지로 갈 수 있도록 변경"
    로그인 안되도 main페이지로 갈 수 있도록 변경

--- thirtyone/src/main/java/com/oti/thirtyone/controller/HomeController.java
+++ thirtyone/src/main/java/com/oti/thirtyone/controller/HomeController.java
@@ -1,14 +1,38 @@
 package com.oti.thirtyone.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.oti.thirtyone.dto.EmployeesDto;
+import com.oti.thirtyone.security.EmployeeDetails;
+import com.oti.thirtyone.service.DepartmentService;
+
+import lombok.extern.log4j.Log4j2;
+
 @Controller
+@Log4j2
 public class HomeController {
 
+	@Autowired
+	DepartmentService deptService;
+	
 	@GetMapping("/")
-	public String home(Model model){
+	public String home(Model model, Authentication authentication){
+		if(authentication != null) {
+			EmployeeDetails empDetails = (EmployeeDetails) authentication.getPrincipal();
+			EmployeesDto empDto = empDetails.getMember();
+			String empName= empDto.getEmpName();
+			String empPosition = empDto.getPosition();
+			int deptId = empDto.getDeptId();
+			
+			String deptName = deptService.getDeptName(deptId);
+			
+			model.addAttribute("empNamePosition", empName + " " + empPosition);
+			model.addAttribute("empDept", deptName);
+		}
 		model.addAttribute("title", "");
 		model.addAttribute("selectedTitle", "home");
 		return "home";
