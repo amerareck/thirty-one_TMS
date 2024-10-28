b066c6f53f8b0d5c6cbb67b796943e54e1a7b6b3
361_seok <jws4858@gmail.com>
2024-10-28 17:20:49 +0900

    Update: "security 경로 변경"
    security 경로 변경

--- thirtyone/src/main/java/com/oti/thirtyone/security/AuthenticationSuccessHandler.java
+++ thirtyone/src/main/java/com/oti/thirtyone/security/AuthenticationSuccessHandler.java
@@ -1,27 +1,36 @@
 package com.oti.thirtyone.security;
 
 import java.io.IOException;
+import java.util.Set;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.security.core.Authentication;
-import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class AuthenticationSuccessHandler 
 
-	extends SavedRequestAwareAuthenticationSuccessHandler{
+	extends SimpleUrlAuthenticationSuccessHandler{
 	
 	@Override
 	public void onAuthenticationSuccess(
 			HttpServletRequest request, 
 			HttpServletResponse response,
 			Authentication authentication) throws ServletException, IOException {
-		log.info("실행");
+		Set<String> roles = AuthorityUtils.authorityListToSet(authentication.getAuthorities());
+		
+		if( roles.contains("ROLE_ADMIN")) {
+			setDefaultTargetUrl("/admin");
+		}else {
+			setDefaultTargetUrl("/home");
+		}
+			
 		super.onAuthenticationSuccess(request, response, authentication);
 		
 	}
