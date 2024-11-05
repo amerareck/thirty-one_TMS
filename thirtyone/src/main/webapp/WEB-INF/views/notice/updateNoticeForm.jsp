0c4054da1fc59242b802bc665cb8f7a1b4a455c8
jihye <xxxxxzzz@naver.com>
2024-11-05 18:37:15 +0900

    Update: "부서이름 출력"

--- thirtyone/src/main/webapp/WEB-INF/views/notice/updateNoticeForm.jsp
+++ thirtyone/src/main/webapp/WEB-INF/views/notice/updateNoticeForm.jsp
@@ -5,132 +5,156 @@
 <meta name='viewport' content='width=device-width, initial-scale=1'>
 
 <!-- include libraries(jQuery, bootstrap) -->
-<link href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" rel="stylesheet">
+<!-- <link
+	href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css"
+	rel="stylesheet">
 <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+<script
+	src="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script> -->
 
 <!-- include summernote css/js -->
-<link href="https://cdn.jsdelivr.net/npm/summernote@0.9.0/dist/summernote.min.css" rel="stylesheet">
-<script src="https://cdn.jsdelivr.net/npm/summernote@0.9.0/dist/summernote.min.js"></script>
+<link
+	href="https://cdn.jsdelivr.net/npm/summernote@0.9.0/dist/summernote.min.css"
+	rel="stylesheet">
+<script
+	src="https://cdn.jsdelivr.net/npm/summernote@0.9.0/dist/summernote.min.js"></script>
 
 <link rel="stylesheet" type="text/css"
 	href="${pageContext.request.contextPath}/resources/css/noticeWriteForm.css">
 
 <div class="content-box">
-			<div class="main-container" >
-				<p class="title-31">${title}</p>
+	<div class="main-container">
+		<p class="title-31">${title}</p>
 
-<div class="cardBox">
-	<div class="text">
-		써리원의 사내 공지사항을 작성합니다.
-		<button type="button" class="btn list"
-			onclick="location.href='${pageContext.request.contextPath}/notice/noticeList'">목록</button>
-	</div>
-
-	<div class="content">
-		<div class="line" style="border: 2.5px solid #F0F0F0;"></div>
-
-		<form id="contentForm" method="POST" action="updateNotice" enctype="multipart/form-data">
-			<div class="noticeTitle">
-				<div class="notice-title">
-					<img class="star-icon"
-						src="${pageContext.request.contextPath}/resources/image/star_icon.png"
-						alt="star" style="width: 9px" />제목
-				</div>
-				<input type="text" id="titleBox" name="noticeTitle" value="${notice.noticeTitle}"/>
+		<div class="cardBox">
+			<div class="text">
+				써리원의 사내 공지사항을 작성합니다.
+				<button type="button" class="btn list"
+					onclick="location.href='${pageContext.request.contextPath}/notice/noticeList'">목록</button>
 			</div>
-			<div class="line"></div>
 
-			<div class="noticeTarget">
-				<div class="notice-target" >공지 대상</div>
-				<button type="button" class="button-small search"
-					data-bs-toggle="modal" data-bs-target="#exampleModal">찾기</button>
+			<div class="content">
+				<div class="line" style="border: 2.5px solid #F0F0F0;"></div>
+
+				<form id="contentForm" method="POST" action="updateNotice"
+					enctype="multipart/form-data">
+					<div class="noticeTitle">
+						<div class="notice-title">
+							<img class="star-icon"
+								src="${pageContext.request.contextPath}/resources/image/star_icon.png"
+								alt="star" style="width: 9px" />제목
+						</div>
+						<input type="text" id="titleBox" name="noticeTitle"
+							value="${notice.noticeTitle}" />
+					</div>
+					<div class="line"></div>
+
+					<div class="noticeTarget">
+						<div class="notice-target">공지 대상</div>
+						<button type="button" class="button-small search" id="deptSearch"
+							data-bs-toggle="modal" data-bs-target="#ganadaramabasa">찾기</button>
 
 
-				<!-- Modal -->
-				<div class="modal fade" id="exampleModal" tabindex="-1"
-					aria-labelledby="exampleModalLabel" aria-hidden="true">
-					<div class="modal-dialog">
-						<div class="modal-content">
-							<div class="modal-header">
-								<div class="modal-title" id="exampleModalLabel">부서 찾기</div>
-								<button type="button" class="btn-close" data-bs-dismiss="modal"
-									aria-label="Close"></button>
+						<div class="targetBox" id="result">
+							<c:choose>
+								<c:when test="${empty noticeDeptList}">
+								미선택 시, 전체직원에게 공지가 노출됩니다.
+								</c:when>
+								<c:when test="${not empty noticeDeptList}">
+									<c:forEach var="dept" items="${noticeDeptList}">
+										<div class="target" style="margin-right: 5px;">${dept.deptId}</div>
+									</c:forEach>
+								</c:when>
+							</c:choose>
+
+						</div>
+
+						<div class="notice-target">중요도</div>
+						<div class="custom-select" style="width: 75px;">
+							<select class="form-select" id="formSelect"
+								name="noticeImportant" required>
+								<option value="">선택</option>
+								<option value="0">기본</option>
+								<option value="1">중요</option>
+							</select>
+						</div>
+					</div>
+
+					<div class="line"></div>
+
+					<div class="plusFile">
+						<div class="plus-file">첨부파일</div>
+
+						<div class="fileContent" href="javascript:" id="dropZone">
+							<div class="fileBox">
+								<c:forEach var="file" items="${noticeFiles}">
+									<input type="hidden" name="noticeFileName"
+										value="${file.noticeFileName}">
+								</c:forEach>
+								<p>
+									<img
+										src="${pageContext.request.contextPath}/resources/image/plusFile_icon.png"
+										alt="plusFile" style="width: 44px" id="preview" /> 마우스로 파일을
+									끌어놓으세요.
+								</p>
 							</div>
-							<div class="modal-body">
+							<div class="line file"></div>
+							<input type="file" id="uploadFile" class="button-small upload"
+								value="내 PC" name="attachFile" multiple>
 
-							<%-- <c:forEach var="dept" items="${deptItem}">  --%>
-								<!-- 체크 -->
-								<div class="form-check">
-									<input class="form-check-input" type="checkbox" name="deptId" value="${dept.deptName}"
-										id="flexCheckDefault" onclick="getCheckboxValue()"><label class="form-check-label"
-										for="flexCheckDefault"></label>
+						</div>
+					</div>
+
+					<div class="line"></div>
+					<div class="contentBtn">
+						<!-- <div contenteditable="true"> -->
+						<textarea id="summernote" name="noticeContent">${notice.noticeContent}</textarea>
+					</div>
+					<div class="noticeButton">
+						<button class="button-medium cancel"
+							onclick="location.href='${pageContext.request.contextPath}/notice/noticeDetail?noticeId=${notice.noticeId}'">취소</button>
+						<input type="submit" class="button-medium" id="save" name="action"
+							value="수정"></input>
+					</div>
+					<input type="hidden" name="noticeId" value="${notice.noticeId}" id="noticeId">
+					<input type="hidden" name="empId" value="${employees.empId}">
+
+					<!-- Modal -->
+					<div class="modal fade" id="exampleModal" tabindex="-1"
+						aria-labelledby="exampleModalLabel" aria-hidden="true">
+						<div class="modal-dialog">
+							<div class="modal-content">
+								<div class="modal-header">
+									<div class="modal-title" id="exampleModalLabel">부서 찾기</div>
+									<button type="button" class="btn-close" data-bs-dismiss="modal"
+										aria-label="Close"></button>
 								</div>
-							<%-- </c:forEach> --%>
+								<div class="modal-body" id="modalBody">
 
-							</div>
-							<div class="modal-footer">
-								<button type="button" class="button-medium cancel"
-									data-bs-dismiss="modal">취소</button>
-								<button type="submit" class="button-medium save" onclick="getCheckboxValue()">확인</button>
+									<%-- <c:forEach var="deptName" items="${deptName}">  --%>
+									<!-- 체크 -->
+									<div class="form-check">
+										<input class="form-check-input" type="checkbox" name="deptId"
+											value="${dept.deptId}" id="${dept.deptName}"
+											onclick="getCheckboxValue()"><label
+											class="form-check-label" for="${dept.deptName}"></label>
+									</div>
+									<%--  </c:forEach> --%>
+
+								</div>
+								<div class="modal-footer">
+									<button type="button" class="button-medium cancel"
+										data-bs-dismiss="modal">취소</button>
+									<input type="submit" class="button-medium save"
+										onclick="getCheckboxValue()" id="getCheckboxValue" value="확인">
+								</div>
 							</div>
 						</div>
 					</div>
-				</div>
-
-
-
-				<div class="targetBox" id="result">미선택 시, 전체직원에게 공지가 노출됩니다.</div>
-
-				<div class="notice-target">중요도</div>
-				<div class="custom-select" style="width: 75px;">
-					<select class="form-select" id="formSelect" name="noticeImportant" required>
-						<%-- <c:choose test="${notice.notieImportant == '0'">
-							${notice.noticeImportant}</c:choose> --%>				
-						<option value="">선택</option>
-						<option value="0">기본</option>
-						<option value="1">중요</option>
-						<%-- <option value="${notice.noticeImportant} active">${notice.noticeImportant}</option> --%>
-					</select>
-				</div>
+				</form>
 			</div>
-			<div class="line"></div>
-
-			<div class="plusFile">
-				<div class="plus-file">첨부파일</div>
-
-				<div class="fileContent" href="javascript:" id="dropZone">
-						<div class="fileBox">
-							<c:forEach var="file" items="${noticeFiles}">
-							<input type="hidden" name="noticeFileName" value="${file.noticeFileName}">
-							</c:forEach>
-							<p><img
-								src="${pageContext.request.contextPath}/resources/image/plusFile_icon.png"
-								alt="plusFile" style="width: 44px" id="preview" />
-							마우스로 파일을 끌어놓으세요.</p>
-						</div>
-						<div class="line file"></div> <input type="file" id="uploadFile" 
-						class="button-small upload" value="${noticeFile.noticeFileId}" accept="image/*" name="attachFile" multiple>
-						
-				</div>
-			</div>
-
-			<div class="line"></div>
-			<div class="contentBtn">
-				<!-- <div contenteditable="true"> -->
-				<textarea id="summernote" name="noticeContent">${notice.noticeContent}</textarea>
-			</div>
-			<div class="noticeButton">
-				<button class="button-medium cancel"
-					onclick="location.href='${pageContext.request.contextPath}/notice/noticeDetail?noticeId=${notice.noticeId}'">취소</button>
-				<input type="submit" class="button-medium" id="save" name="action" value="수정"></input>
-			</div>
-			<input type="hidden" name="noticeId" value="${notice.noticeId}">
-			<input type="hidden" name="empId" value="${employees.empId}">
-	</form>
+		</div>
 	</div>
-</div>
-<script
-	src="${pageContext.request.contextPath}/resources/js/notice/updateNoticeForm.js"></script>
-<script src="https://cdn.ckeditor.com/4.25.0/standard/ckeditor.js"></script>
-<%@ include file="/WEB-INF/views/common/footer.jsp"%>
+	<script
+		src="${pageContext.request.contextPath}/resources/js/notice/updateNoticeForm.js"></script>
+	<%@ include file="/WEB-INF/views/common/footer.jsp"%>
--- thirtyone/src/main/webapp/resources/js/notice/noticeWriteForm.js
+++ thirtyone/src/main/webapp/resources/js/notice/noticeWriteForm.js
@@ -1,306 +1,3 @@
-/*// 체크박스
-	function getCheckboxValue() {   
-	   console.log("호출됨");
-	   
-	   const deptNameCheck = $('input[name="deptId"]:checked');
-	   console.log(deptNameCheck);
-	   const result = document.getElementById("result");
-	   const selectedDeptName = [];
-	   
-	   result.innerHTML = "";
-	   
-	   if (deptNameCheck.length === 0) {
-	      result.innerHTML = "미선택 시, 전체직원에게 공지가 노출됩니다.";
-	   } else {
-	      deptNameCheck.each(function() {
-	      selectedDeptName.push($(this).next('label').text());
-	         });
-	         result.innerHTML = selectedDeptName.join(", ");
-	      }
-	}
-	   $("#getCheckboxValue").click(function(event) {
-	      event.preventDefault();
-	      getCheckboxValue();
-	   $("#exampleModal").modal("hide");
-	      
-	});
-
-document.addEventListener("DOMContentLoaded", function() {
-	
-	const contentForm = document.getElementById("contentForm");
-	const uploadFile = document.getElementById("uploadFile"); 
-	const fileBoxDisplay = document.getElementById("fileBox");
-	let filesArray = [];
-
-
-$('#deptSearch').click(openDeptModal); 
-   console.log("찾기");
-   
-   
-   function openDeptModal() {
-		
-		   $.ajax({
-		      method: 'GET',
-	      url: contextPath +'/notice/deptList',
-	      success: function(data) {
-	         console.log("AJAX 성공", data);
-	         const modalBody = document.getElementById("modalBody");
-	         modalBody.innerHTML = "";
-	         data.forEach(dept => {
-	            const deptCheck = document.createElement("div");
-	            deptCheck.classList.add('form-check');
-	            deptCheck.innerHTML = `
-	            <input class="form-check-input" type="checkbox" name="deptId" value="${dept.deptId}"
-	            id="dept_${dept.deptName}">
-	            <label class="form-check-label" for="dept_${dept.deptName}">${dept.deptName}</label>
-	            `;
-	            modalBody.appendChild(deptCheck);
-	         });   
-	         
-	         $("#exampleModal").modal("show");
-	   },
-	      error: function(xhr, status, error) {
-	         console.error('Error', xhr.status, error);      
-		      }
-	   });
-   }
-   
-
-function submitForm() {
-   	const formData = new FormData(contentForm);
-   	filesArray.forEach(file => formData.append('attachFile', file)); // 파일 추가
-
-   	const selectedDeptIds = getCheckedDeptIds();
-   	const noticeId = 1;
-   	
-   	selectedDeptIds.forEach(deptId => {
-   		formData.append('deptId[]', deptId);
-   	});
-   	formData.append('noticeId', noticeId);
-   	
-   	console.log("부서 ID:", [...formData]);
-   	}
-
-
-	$.ajax({
-		method: 'POST',
-		url: contextPath + '/notice/noticeWrite',
-		data: formData,
-		processData: false,
-		contentType: false,
-		success: function(response) {
-			console.log("작성 AJAX 성공", response);
-			location.href = contextPath + '/notice/noticeList';
-		},
-		
-		error: function(xhr, status, error) {
-			console.error('Error', xhr.status, error);
-			console.log(xhr.responseText);
-		}
-	});
-	}
-  	
-   	// 체크박스
-	function getCheckboxValue() {   
-		   console.log("호출됨");
-		   
-		   const deptNameCheck = $('input[name="deptId"]:checked');
-		   console.log(deptNameCheck);
-		   const result = document.getElementById("result");
-		   const selectedDeptName = [];
-		   
-		   result.innerHTML = "";
-		   
-		   if (deptNameCheck.length === 0) {
-		      result.innerHTML = "미선택 시, 전체직원에게 공지가 노출됩니다.";
-		   } else {
-		      deptNameCheck.each(function() {
-		      selectedDeptName.push($(this).next('label').text());
-		         });
-		         result.innerHTML = selectedDeptName.join(", ");
-		      }
-		}
-		   $("#getCheckboxValue").click(function(event) {
-		      event.preventDefault();
-		      getCheckboxValue();
-		   $("#exampleModal").modal("hide");
-		      
-		});
-
-	document.addEventListener("DOMContentLoaded", function() {
-		
-		const contentForm = document.getElementById("contentForm");
-		const uploadFile = document.getElementById("uploadFile"); 
-		const fileBoxDisplay = document.getElementById("fileBox");
-		let filesArray = [];
-
-		
-
-	$('#deptSearch').click(openDeptModal); 
-	   console.log("찾기");
-	   
-	   
-	   function openDeptModal() {
-			
-			   $.ajax({
-			      method: 'GET',
-		      url: contextPath +'/notice/deptList',
-		      success: function(data) {
-		         console.log("AJAX 성공", data);
-		         const modalBody = document.getElementById("modalBody");
-		         modalBody.innerHTML = "";
-		         data.forEach(dept => {
-		            const deptCheck = document.createElement("div");
-		            deptCheck.classList.add('form-check');
-		            deptCheck.innerHTML = `
-		            <input class="form-check-input" type="checkbox" name="deptId" value="${dept.deptId}"
-		            id="dept_${dept.deptName}">
-		            <label class="form-check-label" for="dept_${dept.deptName}">${dept.deptName}</label>
-		            `;
-
-		            modalBody.appendChild(deptCheck);
-		         });   
-
-		         $("#exampleModal").modal("show");
-		   },
-		      error: function(xhr, status, error) {
-		         console.error('Error', xhr.status, error);      
-			      }
-		   });
-	   }
-
-
-	   function getCheckedDeptIds() {
-		const deptNameCheck = $('input[name="deptId"]:checked');
-		const selectedDeptIds = [];
-		
-		deptNameCheck.each(function() {
-			selectedDeptIds.push($(this).val());
-		});
-		return selectedDeptIds;
-	   }
-
-	
-   contentForm.addEventListener("submit", function(e) {
-      e.preventDefault();
-
-      if (contentValidChk()) {
-   	   submitForm();
-      }
-   });
-
-
-   $('#summernote').summernote();
-   
-
-// 유효성 검사
-function contentValidChk() {
-	var titleBox = document.getElementById("titleBox").value;
-   var summernote = $("#summernote").summernote('code');
-   
-   if (!titleBox) {
-      alert("제목을 입력해주세요.");
-      return false;
-   }
-
-   if (!summernote || summernote === '<p><br></p>') {
-      alert("내용이 비어있습니다. 내용을 입력해주세요.");
-      return false;
-   }
-   return true;
-}
-
-const dropZone = document.querySelector("#dropZone");
-dropZone.addEventListener('dragover', (e) => e.preventDefault());
-dropZone.addEventListener('drop', handleDrop);
-
-function handleDrop(event) {
-	event.preventDefault();
-    event.stopPropagation();
-	const files = event.dataTransfer.files;
-	handleFiles(files);
-}
-
-// 파일 처리 함수
-function handleFiles(files) {
-	Array.from(files).forEach(file => {
-		if (file.type.match('image.*')) {
-			
-			if (!filesArray.includes(file)) {
-				filesArray.push(file);
-				const reader = new FileReader();
-				reader.onload = (event) => {
-					const imgElement = document.createElement("img");
-					imgElement.src = event.target.result;
-					imgElement.style.width = '100px';
-					fileBoxDisplay.appendChild(imgElement);
-			};
-			reader.readAsDataURL(file);
-			}
-		} else {
-			alert("이미지 파일이 아닙니다.");
-		}
-	});
-	displayFileList();
-}
-
-
-//파일 목록 표시
-function displayFileList() {
-	fileBoxDisplay.innerHTML = '';
-	if (filesArray.length === 0) {
-		fileBoxDisplay.innerHTML = fileBoxDisplay.innerHTML = '<p><img src="/thirtyone/resources/image/plusFile_icon.png" alt="plusFile" style="width: 44px" id="preview" />&nbsp;마우스로 파일을 끌어놓으세요.</p>';
-	} else {
-		filesArray.forEach((file, index) => {
-			 const imgElement = document.createElement("img");
-	            imgElement.src = URL.createObjectURL(file);
-	            imgElement.style.width = '100px';
-	            imgElement.style.margin = '5px';      	         
-	            fileBoxDisplay.appendChild(imgElement)
-	            // 삭제 버튼 추가
-	            const removeButton = document.createElement("button");
-	            removeButton.innerHTML = '<img src="/thirtyone/resources/image/cancel_icon.png" style="width: 30px">';	            
-	            removeButton.style.background = 'transparent';
-	            removeButton.style.border = 'none';
-	            removeButton.style.cursor = 'pointer';
-	            removeButton.style.margin = '-40px 0px 20px -45px';
-	            
-	            removeButton.addEventListener("click", (event) => {
-	               event.preventDefault();
-	               event.stopPropagation();
-	               removeFile(index)
-	            });   
-	            fileBoxDisplay.appendChild(removeButton);
-	         });
-	      
-	   }
-}
-
-//파일 삭제 함수
-function removeFile(index) {
-   filesArray.splice(index, 1); // 배열에서 파일 제거
-   displayFileList(); // 이미지 미리보기 업데이트
-}
-
-uploadFile.addEventListener("change", (event) => {
-	const tempList = Array.from(event.target.files);
-	tempList.forEach(file => {
-		filesArray.push(file);
-	});
-	displayFileList();
-});
-
-document.querySelector(".fileBox").addEventListener("click", () => {
-	uploadFile.click();
-	});
-});
-
-*/
-
-
-
-
-
 document.addEventListener("DOMContentLoaded", function() {
 	const contentForm = document.getElementById("contentForm");
 	const uploadFile = document.getElementById("uploadFile");
@@ -367,11 +64,21 @@ document.addEventListener("DOMContentLoaded", function() {
 		}).get();
 	}
 	
+	
+	//폼 제출 이벤트 리스너
+	contentForm.addEventListener("submit", function(event) {
+		event.preventDefault();
+        event.stopPropagation();
+        const formData = new FormData(contentForm);				
+
+        if (contentValidChk()) {
+			submitForm();
+		}
+	});
 
 	
 	//공지 작성 폼 제출
 	function submitForm() {
-		const formData = new FormData(contentForm);				
 		console.log("Form Data:", [...formData]);
 		
 		filesArray.forEach(file => formData.append('attachFile', file));
@@ -398,14 +105,7 @@ document.addEventListener("DOMContentLoaded", function() {
 			}
 		});
 	}
-	
-	//폼 제출 이벤트 리스너
-	contentForm.addEventListener("submit", function(e) {
-		e.preventDefault();
-		if (contentValidChk()) {
-			submitForm();
-		}
-	});
+
 	
 	$('#summernote').summernote();
 	
@@ -431,15 +131,36 @@ document.addEventListener("DOMContentLoaded", function() {
 	dropZone.addEventListener('dragover', (e) => e.preventDefault());
 	dropZone.addEventListener('drop', handleDrop);
 	
-	uploadFile.addEventListener("change", (event) => {
+	/*uploadFile.addEventListener("change", (event) => {
 		filesArray = Array.from(event.target.files);
 		let tempList = Array.from(event.target.files); // FileList를 배열로 변환
 		   tempList.forEach(function (file, index)  {
 		      filesArray.push(file);
 		});
 		displayFileList();
-	});
+	});*/
 	
+	uploadFile.addEventListener("change", () => {
+		// filesArray = Array.from(event.target.files); => 파일이 1개씩 들어오면 filesArray의 길이는
+		// 영원히 1;
+		// 파일이 들어올때마다 배열로 변화시켜 filesArray에 추가한다면 filesArray의 값은 입력된 파일의 개수만큼 증가된다.
+	   let tempList = Array.from(event.target.files); // FileList를 배열로 변환
+	   tempList.forEach(function (file, index)  {
+	      filesArray.push(file);
+	   })
+	   
+	    // 새로운 파일 입력 요소 생성
+	    const newFileInput = document.createElement("input");
+	    newFileInput.type = "file";
+	    newFileInput.name = "attachFile"; // 'files' 배열로 서버에 전달되도록 동일한 name 사용
+	    newFileInput.multiple = true; // 여러 파일 선택 허용
+
+	    // 새 파일 입력 요소를 폼에 추가
+	    /* document.getElementById("dropZone").appendChild(newFileInput); */	   
+	   console.log(filesArray);
+	   displayFileList();
+	});
+
 	document.querySelector(".fileBox").addEventListener("click", () => {
 		uploadFile.click();
 	});
