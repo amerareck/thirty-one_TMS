ce67300c429965075e465b08857e83ccde4e9404
Sunghyun <tjdgus510@gmail.com>
2024-11-13 02:30:39 +0900

    feat: 전자 결재 검색 로직 구현 완료
    전자결재에 대한 검색 로직을 구현하였습니다.
    폼 형식으로 간단하게 js와 유형 폼태그를 추가하고
    메서드에 type만 추가하면 유형을 쉽게 추가할 수 있어 빠르게 수정 및 변경 가능합니다.

--- thirtyone/src/main/java/com/oti/thirtyone/controller/ApprovalController.java
+++ thirtyone/src/main/java/com/oti/thirtyone/controller/ApprovalController.java
@@ -261,6 +261,11 @@ public class ApprovalController {
 			
 			draftList = approvalService.getDepartmentsDrafts(myInfo.getDeptId());
 		}
+		if(param.getSearch() != null) {
+			draftList = getPickupSearchList(param, draftList);
+			model.addAttribute("search", true);
+			model.addAttribute("paramObj", param);
+		}
 		
 		Pager pager = new Pager(7, 5, draftList.size(), param.getPageNo());
 		List<ApprovalDTO> pageNoDraftList = draftList.stream()
@@ -333,13 +338,21 @@ public class ApprovalController {
 		
 		model.addAttribute("selectedSub", "submitted");
 		model.addAttribute("title", "기결/회수");
+		model.addAttribute("urlName", "submitted");
 		param.setPageNo(param.getPageNo() == 0 ? 1 : param.getPageNo());
 		
 		if(param.getType()!=null && param.getType().equals("retrieval")) {
 			model.addAttribute("activePage", "retrieval");
 			model.addAttribute("sectionTitle", "결재 회수 목록");
 			
-			List<ApprovalDTO> recallList = approvalService.getRecallDocumentListById(auth.getName());
+			List<ApprovalDTO> recallTotalList = approvalService.getRecallDocumentListById(auth.getName());
+			if(param.getSearch() != null) {
+				recallTotalList = getPickupSearchList(param, recallTotalList);
+				model.addAttribute("search", true);
+				model.addAttribute("paramObj", param);
+			}
+			List<ApprovalDTO> recallList = recallTotalList;
+			
 			Pager pager = new Pager(7, 5, recallList.size(), param.getPageNo());
 			recallList.removeIf(item -> {
 	            int index = recallList.indexOf(item)+1;
@@ -390,7 +403,14 @@ public class ApprovalController {
 			model.addAttribute("activePage", "submitted");
 			model.addAttribute("sectionTitle", "결재 상신 목록");
 			
-			List<ApprovalDTO> approvalList = approvalService.getDraftDocumentById(auth.getName());
+			List<ApprovalDTO> approvalTotalList = approvalService.getDraftDocumentById(auth.getName());
+			if(param.getSearch() != null) {
+				approvalTotalList = getPickupSearchList(param, approvalTotalList);
+				model.addAttribute("search", true);
+				model.addAttribute("paramObj", param);
+			}
+			List<ApprovalDTO> approvalList = approvalTotalList;
+			
 			Pager pager = new Pager(7, 5, approvalList.size(), param.getPageNo());
 			approvalList.removeIf(item -> {
 	            int index = approvalList.indexOf(item)+1;
@@ -439,6 +459,29 @@ public class ApprovalController {
 		return "approval/approvalSubmitted";
 	}
 	
+	private List<ApprovalDTO> getPickupSearchList(PageParam param, List<ApprovalDTO> recallList) {
+		String type = param.getSearch();
+		log.info("##### param: "+param.toString());
+		List<ApprovalDTO> result = null;
+		switch(type) {
+			case "draftTitle" : 
+				result = recallList.stream().filter(item -> item.getDocTitle().contains(param.getKeyword())).collect(Collectors.toList());
+				break;
+			case "draftAuthor" : 
+				result = recallList.stream().filter(item -> item.getEmpName().contains(param.getKeyword())).collect(Collectors.toList());
+				break;
+			case "draftType" :
+				result = recallList.stream().filter(item -> item.getDocFormName().equals(param.getKeyword())).collect(Collectors.toList());
+				break;
+			case "draftState" :
+				result = recallList.stream().filter(item -> item.getDocAprStatus().equals(param.getKeyword())).collect(Collectors.toList());
+				break;
+			default :
+				result = new ArrayList<>();
+		}
+		return result;
+	}
+
 	@GetMapping("/before")
 	public String getBeforePage(PageParam param, Authentication auth, Model model) {
 		log.info("실행");
@@ -1154,4 +1197,5 @@ public class ApprovalController {
 			ioex.printStackTrace();
 		}
 	}
+	
 }
